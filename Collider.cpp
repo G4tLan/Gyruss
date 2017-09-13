38b9adb459b660ddd6e6d536595abdefcd0cd715
1043882 <1043882@students.wits.ac.za>
2017-09-13 12:12:22 +0200

    added Collider interface

--- Collider.cpp
+++ Collider.cpp
@@ -1,10 +1,26 @@
 #include "Collider.h"
 
-Collider::Collider()
+Collider::Collider(sf::Vector2f& parentPosition, sf::Vector2f& parentBounds):
+_2Dcollider{parentPosition,parentBounds}
 {
+	
 }
 
 Collider::~Collider()
 {
 }
 
+
+void Collider::update(sf::Vector2f& parentPosition, sf::Vector2f& parentBounds){
+	sf::FloatRect tempRect{parentPosition,parentBounds};
+	_2Dcollider = tempRect;
+}
+
+
+bool  Collider::collided(vector<sf::FloatRect>& gameObjects){
+	for(auto bounds : gameObjects){
+		if(_2Dcollider.intersects(bounds))
+			return true;
+	}
+	return false;
+}
--- Collider.h
+++ Collider.h
@@ -1,12 +1,18 @@
 #ifndef COLLIDER_H
 #define COLLIDER_H
+#include <SFML/Graphics.hpp>
+#include <vector>
+using namespace std;
 
 class Collider
 {
 public:
-	Collider();
+	Collider(sf::Vector2f& parentPosition, sf::Vector2f& parentBounds);
+	bool collided(vector<sf::FloatRect>& gameObjects);
+	void update(sf::Vector2f& parentPosition, sf::Vector2f& parentBounds);
 	~Collider();
-
+private:
+	sf::FloatRect _2Dcollider;
 };
 
 #endif // COLLIDER_H
